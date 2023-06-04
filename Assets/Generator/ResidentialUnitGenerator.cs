60fe8850fdffe965d14aebe08c93fa570a83f3ea
adamsocki <socki.adam@gmail.com>
2023-06-03 20:18:45 -0500

    camera render for unit and simple gen

--- Assets/Generator/ResidentialUnitGenerator.cs
+++ Assets/Generator/ResidentialUnitGenerator.cs
@@ -1,22 +1,43 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using static ResidentialUnitGenerator;
 
+public class Unit_Room
+{
+    public int ID { get; set; }
+    public List<Room> Rooms { get; set; }
+
+    public Unit_Room(int id)
+    {
+        ID = id;
+        Rooms = new List<Room>();
+    }
+
+    public void AddRoom(Room room)
+    {
+        Rooms.Add(room);
+    }
+}
 
 
 //[ExecuteInEditMode]
 public class ResidentialUnitGenerator : MonoBehaviour
 {
 
+    public GameObject parent;
+
     public class Room
     {
         public Vector3 Position { get; set; }
         public Vector3 Size { get; set; }
+        public Vector3 UnitsToRender { get; set; }
 
-        public Room(float x, float y, float z, float width, float height, float depth)
+        public Room(float x, float y, float z, float width, float height, float depth, Vector3 unitsToRender)
         {
             Position = new Vector3(x, y, z);
             Size = new Vector3(width, height, depth);
+            UnitsToRender = new Vector3(unitsToRender.x, unitsToRender.y, unitsToRender.z);
         }
     }
 
@@ -37,14 +58,16 @@ public class ResidentialUnitGenerator : MonoBehaviour
     private void Start()
     {
         float wallHeight = wallPrefab.GetComponent<Renderer>().bounds.size.y;
-        Room initialRoom = new Room(0, 2, 0, 10, wallHeight, 10);
+        Room initialRoom = new Room(parent.transform.position.x, parent.transform.position.y + 2, parent.transform.position.z, 20, wallHeight, 20, new Vector3(1,1,1));
         _rooms.Add(initialRoom);
-        Room nextRoom = new Room(0, 5 + wallHeight, 0, 10, wallHeight, 10);
-        _rooms.Add(nextRoom);
-        for (int i = 0; i < 10; i++)
+       // Room nextRoom = new Room(0, 5 + wallHeight, 0, 10, wallHeight, 10);
+       // _rooms.Add(nextRoom);
+        for (int i = 0; i < 3; i++)
         {
-            //Room roomToSplit = _rooms[Random.Range(0, _rooms.Count)];
-            //SplitRoom(roomToSplit);
+            bool final = false;
+            Room roomToSplit = _rooms[Random.Range(0, _rooms.Count)];
+            
+            SplitRoom(roomToSplit);
         }
 
         foreach (Room room in _rooms)
@@ -65,18 +88,21 @@ public class ResidentialUnitGenerator : MonoBehaviour
         float wallHeight = wallPrefab.GetComponent<Renderer>().bounds.size.y;
         float wallThickness = wallPrefab.GetComponent<Renderer>().bounds.size.z;
 
+        float doorWidth = doorPrefab.GetComponent<Renderer>().bounds.size.x;
         float doorHeight = doorPrefab.GetComponent<Renderer>().bounds.size.y;
+        float doorThickness = doorPrefab.GetComponent<Renderer>().bounds.size.z;
 
-        for (int i = 0; i < room.Size.x; i++)
+
+        for (int i = 0; i < room.UnitsToRender.x; i++)
         {
-            GameObject wall_x1 = Instantiate(wallPrefab, new Vector3(room.Position.x + i, room.Position.y + wallHeight / 2, room.Position.z + wallThickness / 2), Quaternion.identity);
-            GameObject wall_x2 = Instantiate(wallPrefab, new Vector3(room.Position.x + i, room.Position.y + wallHeight / 2, room.Position.z + room.Size.z - wallThickness / 2), Quaternion.identity);
+            GameObject wall_x1 = Instantiate(wallPrefab, new Vector3(room.Position.x + i, room.Position.y + wallHeight / 2, room.Position.z + wallThickness / 2), Quaternion.identity, parent.transform);
+            GameObject wall_x2 = Instantiate(wallPrefab, new Vector3(room.Position.x + i, room.Position.y + wallHeight / 2, room.Position.z + room.Size.z - wallThickness / 2), Quaternion.identity, parent.transform);
         }
 
-        for (int i = 0; i < room.Size.z; i++)
+        for (int i = 0; i < room.UnitsToRender.z; i++)
         {
-            GameObject wall_y1 = Instantiate(wallPrefab, new Vector3(room.Position.x - wallWidth / 2 - wallThickness / 2, room.Position.y + wallHeight / 2, room.Position.z + i + wallWidth / 2), Quaternion.Euler(0, 90, 0));
-            GameObject wall_y2 = Instantiate(wallPrefab, new Vector3(room.Position.x + room.Size.x - wallWidth / 2 - wallThickness / 2, room.Position.y + wallHeight / 2, room.Position.z + i + wallWidth / 2), Quaternion.Euler(0, 90, 0));
+            GameObject wall_z1 = Instantiate(wallPrefab, new Vector3(room.Position.x - wallWidth / 2 - wallThickness / 2, room.Position.y + wallHeight / 2, room.Position.z + i + wallWidth / 2), Quaternion.Euler(0, 90, 0), parent.transform);
+            GameObject wall_z2 = Instantiate(wallPrefab, new Vector3(room.Position.x + room.Size.x - wallWidth / 2 - wallThickness / 2, room.Position.y + wallHeight / 2, room.Position.z + i + wallWidth / 2), Quaternion.Euler(0, 90, 0), parent.transform);
         }
 
         float floorWidth = floorPrefab.GetComponent<Renderer>().bounds.size.z;
@@ -89,8 +115,73 @@ public class ResidentialUnitGenerator : MonoBehaviour
         }
 
         // Instantiate door at the center of the room, you may want to update this according to your specific needs
-        GameObject door = Instantiate(doorPrefab, new Vector3(room.Position.x + room.Size.x / 2, room.Position.y + doorHeight / 2, room.Position.z + wallThickness / 2), Quaternion.identity);
+        //bool doorDirection = Random.value < 0.5f;
+
+        //if (doorDirection) 
+        //{
+        //    GameObject door = Instantiate(doorPrefab, new Vector3(room.Position.x + room.Size.x / 2, room.Position.y + doorHeight / 2, room.Position.z + wallThickness / 2), Quaternion.identity);
+        //}
+        //else
+        //{
+        //    GameObject door = Instantiate(doorPrefab, new Vector3(room.Position.x - doorWidth / 2 - wallThickness / 2, room.Position.y + doorHeight / 2, room.Position.z + room.Size.z / 2), Quaternion.Euler(0, 90, 0));
+        //}
     }
+     
+
+    private void SplitRoom(Room room)
+    {
+        float wallWidth = wallPrefab.GetComponent<Renderer>().bounds.size.x;
+        float wallHeight = wallPrefab.GetComponent<Renderer>().bounds.size.y;
+        float wallThickness = wallPrefab.GetComponent<Renderer>().bounds.size.z;
+
+        float doorWidth = doorPrefab.GetComponent<Renderer>().bounds.size.x;
+        float doorHeight = doorPrefab.GetComponent<Renderer>().bounds.size.y;
+        float doorThickness = doorPrefab.GetComponent<Renderer>().bounds.size.z;
+
+        // Decide whether to split vertically or horizontally
+        // This can be more advanced, for example by checking the proportions of the room
+        bool splitVertically = Random.value < 0.5f;
+
+        // The room will be split in the middle, either vertically or horizontally
+        // For simplicity, the new rooms will be of equal size, but you could add more randomness here
+        float splitPosition;
+        Room room1, room2;
+
+        if (splitVertically)
+        {
+            // Split vertically
+            splitPosition = room.Size.x / 2;
+            Debug.Log("V");
+            Debug.Log("V " + splitPosition);
+            Debug.Log("room.size.x " + room.Size.x);
+            Vector3 unitsForRender = room.Size;
+            unitsForRender.x = splitPosition;
+
+            room1 = new Room(room.Position.x, room.Position.y, room.Position.z, splitPosition , room.Size.y, room.Size.z, unitsForRender);
+            room2 = new Room(room.Position.x + splitPosition , room.Position.y, room.Position.z, room.Size.x - splitPosition , room.Size.y, room.Size.z, unitsForRender);
+        }
+        else
+        {
+            // Split horizontally 
+            splitPosition = room.Size.z / 2;
+            Debug.Log("H");
+            Debug.Log("H " + splitPosition);
+            Debug.Log("room.size.z " + room.Size.z);
+            Vector3 unitsForRender = room.Size;
+            unitsForRender.z = splitPosition;
+
+            room1 = new Room(room.Position.x, room.Position.y, room.Position.z, room.Size.x, room.Size.y, splitPosition + wallThickness, unitsForRender);
+            room2 = new Room(room.Position.x, room.Position.y, room.Position.z + splitPosition , room.Size.x, room.Size.y, room.Size.z - splitPosition, unitsForRender);
+           
+        }
+
+        // Remove the original room and add the two new ones
+        _rooms.Remove(room);
+        _rooms.Add(room1);
+        _rooms.Add(room2);
+    }
+
+
 
     public void GenerateRoom()
     {
